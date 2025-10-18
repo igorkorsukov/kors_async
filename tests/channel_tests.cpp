652c99e5d9dc617d545a1c44ac567d393923bb7a
Igor Korsukov <igor.korsukov@gmail.com>
2025-10-18 16:02:15 +0300

    added tests for channel single thread

---
+++ tests/channel_tests.cpp
@@ -0,0 +1,187 @@
+/*
+MIT License
+
+Copyright (c) 2025 Igor Korsukov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include <thread>
+#include <chrono>
+
+#include <gtest/gtest.h>
+
+#include "../async/channel.h"
+
+using namespace kors::async;
+
+class Channel_Tests : public ::testing::Test
+{
+public:
+};
+
+//! NOTE For example, this could be some kind of service.
+struct Sender {
+    int value = 0;
+    Channel<int> ch;
+
+    void increment()
+    {
+        ++value;
+        ch.send(value);
+    }
+
+    Channel<int> valueChanged() const { return ch; }
+};
+
+//! NOTE This could be some kind of ViewModel that needs to update data in View.
+//! Or some other service
+struct Receiver : public Asyncable {
+    int value = 0;
+    const Sender* sender = nullptr;
+
+    void setSender(const Sender* s)
+    {
+        if (sender) {
+            sender->valueChanged().resetOnReceive(this);
+        }
+
+        sender = s;
+        sender->valueChanged().onReceive(this, [this](const int& val) {
+            value = val;
+        });
+    }
+};
+
+TEST_F(Channel_Tests, SingleThread_Send)
+{
+    Channel<int> ch;
+
+    int receivedVal = 0;
+
+    ch.onReceive(nullptr, [&receivedVal](const int& v) {
+        receivedVal = v;
+    });
+
+    ch.send(42);
+
+    EXPECT_EQ(receivedVal, 42);
+}
+
+TEST_F(Channel_Tests, SingleThread_Send_FromObject)
+{
+    Sender sender;
+
+    int receivedVal = 0;
+
+    sender.valueChanged().onReceive(nullptr, [&receivedVal](const int& v) {
+        receivedVal = v;
+    });
+
+    EXPECT_EQ(receivedVal, 0);
+
+    sender.increment();
+    EXPECT_EQ(receivedVal, 1);
+
+    sender.increment();
+    EXPECT_EQ(receivedVal, 2);
+}
+
+TEST_F(Channel_Tests, SingleThread_Send_Reset)
+{
+    Asyncable asyncable;
+    Sender sender;
+
+    int receivedVal = 0;
+
+    sender.valueChanged().onReceive(&asyncable, [&receivedVal](const int& v) {
+        receivedVal = v;
+    });
+
+    EXPECT_EQ(receivedVal, 0);
+
+    sender.increment();
+    EXPECT_EQ(receivedVal, 1);
+
+    sender.valueChanged().resetOnReceive(&asyncable);
+
+    sender.increment();
+    EXPECT_EQ(receivedVal, 1);
+}
+
+TEST_F(Channel_Tests, SingleThread_Send_Reset_onReceive)
+{
+    Asyncable asyncable;
+    Sender sender;
+
+    int receivedVal = 0;
+
+    sender.valueChanged().onReceive(&asyncable, [&receivedVal, &sender, &asyncable](const int& v) {
+        receivedVal = v;
+        sender.valueChanged().resetOnReceive(&asyncable);
+    });
+
+    EXPECT_EQ(receivedVal, 0);
+
+    sender.increment();
+    EXPECT_EQ(receivedVal, 1);
+
+    sender.increment();
+    EXPECT_EQ(receivedVal, 1);
+}
+
+TEST_F(Channel_Tests, SingleThread_Sender_Receiver)
+{
+    Sender sender;
+    Receiver receiver;
+
+    receiver.setSender(&sender);
+
+    EXPECT_EQ(receiver.value, 0);
+
+    sender.increment();
+    EXPECT_EQ(receiver.value, 1);
+
+    receiver.setSender(nullptr);
+
+    sender.increment();
+    EXPECT_EQ(receiver.value, 1);
+}
+
+TEST_F(Channel_Tests, SingleThread_Sender_MultiReceiver)
+{
+    Sender sender;
+    Receiver receiver1;
+    Receiver receiver2;
+
+    receiver1.setSender(&sender);
+    receiver2.setSender(&sender);
+
+    EXPECT_EQ(receiver1.value, 0);
+    EXPECT_EQ(receiver2.value, 0);
+
+    sender.increment();
+    EXPECT_EQ(receiver1.value, 1);
+    EXPECT_EQ(receiver2.value, 1);
+
+    receiver1.setSender(nullptr);
+
+    sender.increment();
+    EXPECT_EQ(receiver1.value, 1);
+    EXPECT_EQ(receiver2.value, 2);
+}
